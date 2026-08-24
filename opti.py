e69caac2c341c7d156d355edb5d98cec0efa1474
WhoRegisteredAllTheGoodUsernames <38054565+WhoRegisteredAllTheGoodUsernames@users.noreply.github.com>
2026-08-23 22:09:01 -0400

    FEAT: Evita que los primos nuevos queden solos, permite distinta cantidad de primos por turno de forma individual

--- opti.py
+++ opti.py
@@ -2,6 +2,7 @@ import pulp
 from math import floor, ceil
 from collections.abc import Iterable
 
+
 def parse_schedule(schedule, no_blocks):
     parsed_schedule = ''
     for i, day in enumerate('lmxjv'):
@@ -9,23 +10,23 @@ def parse_schedule(schedule, no_blocks):
         for block, availability in enumerate(schedule[i*no_blocks: i*no_blocks+no_blocks]):
             if availability:
                 blocks.append(str(block))
+
         if len(blocks):
             parsed_schedule += f'{day}{",".join(blocks)}'
     return parsed_schedule
 
-def get_schedule(primos: Iterable, no_blocks: int, primos_per_shift: int = 2):
-    # Posibles errores
-    if not (0 < no_blocks <= 8):
-        raise ValueError('La jornada diruna abarca desde el 1-2 hasta el 15-16 (8 turnos)')
+
+def get_schedule(primos: Iterable, new_primos: Iterable, primos_per_shift: Iterable):
     if not len(primos):
         raise ValueError('<primos> está vacío')
-    if primos_per_shift > len(primos):
-        raise ValueError('Se requieren más primos por turno que los primos que existen')
-    
+
+    no_blocks = len(primos_per_shift[0])
+
     # Algunas variables que necesitaremos después...
     days = 'lmxjv'
     shifts = range(len(days)*no_blocks)
-    no_shifts_per_primo = len(days)*no_blocks*primos_per_shift/len(primos)
+    total_shifts = sum(sum(day) for day in primos_per_shift)
+    no_shifts_per_primo = total_shifts/len(primos)
     satisfaction = [pulp.LpVariable(name=f'satisfaction_{shift}', lowBound=0, cat=pulp.LpInteger) for shift in shifts]
     primo_has_shift, primo_shift_weight = {}, {}
     for primo in primos:
@@ -36,25 +37,35 @@ def get_schedule(primos: Iterable, no_blocks: int, primos_per_shift: int = 2):
         for day, _ in enumerate(days):
             for block in range(no_blocks):
                 # OJO: En la BD, los horarios tienen 40 turnos, es decir que cada día tiene 8 bloques, por eso <shift := day*8 + block>
-                
-                #primo_shift_weight[primo].append((primo.desire_schedule[shift := day*8 + block] + 1)*(not primo.bussy_schedule[shift]))
+
+                # primo_shift_weight[primo].append((primo.desire_schedule[shift := day*8 + block] + 1)*(not primo.bussy_schedule[shift]))
                 shift = day * 8 + block
                 primo_shift_weight[primo].append((primo.desire_schedule[shift] + 1) * (not primo.bussy_schedule[shift]))
-
 
     # Definimos el modelo para que maximice la satisfacción por bloque
     model = pulp.LpProblem(sense=pulp.LpMaximize)
     model += sum(satisfaction)
-    
     # Restricciones por turno
     for shift in shifts:
+        block = shift % no_blocks
+        day = int(shift / no_blocks)
+        primos_in_block = primos_per_shift[day][block]
+
         # Tienen que haber <primos_per_shift> primos por turno.
         pps_constraint = sum(primo_has_shift[primo][shift] for primo in primos)
-        model += pulp.LpConstraint(e=pps_constraint, sense=pulp.LpConstraintEQ, rhs=primos_per_shift, name=f'primos_per_shift_{shift}')
-        
+        model += pulp.LpConstraint(e=pps_constraint, sense=pulp.LpConstraintEQ, rhs=primos_in_block, name=f'primos_per_shift_{shift}')
+
         # No te puede tocar un turno donde tienes clases
         maxsat_constraint = satisfaction[shift] - sum(primo_has_shift[primo][shift] * primo_shift_weight[primo][shift] for primo in primos)
         model += pulp.LpConstraint(e=maxsat_constraint, sense=pulp.LpConstraintEQ, rhs=0)
+
+        # No pueden haber primos nuevos solos, y tampoco puede haber más de uno en turno normal
+        np_constraint = sum(primo_has_shift[primo][shift] for primo in new_primos)
+
+        if (primos_in_block == 1):
+            model += pulp.LpConstraint(e=np_constraint, sense=pulp.LpConstraintEQ, rhs=0, name=f'no_new_primos_{shift}')
+        elif (primos_in_block != 0):
+            model += pulp.LpConstraint(e=np_constraint, sense=pulp.LpConstraintLE, rhs=1, name=f'new_primos_in_shift_{shift}')
 
     # Restricciones por primo
     for primo in primos:
@@ -62,11 +73,11 @@ def get_schedule(primos: Iterable, no_blocks: int, primos_per_shift: int = 2):
         no_shifts = pulp.LpVariable(name=f'no_shifts_{primo.rol}', lowBound=floor(no_shifts_per_primo), upBound=ceil(no_shifts_per_primo), cat=pulp.LpInteger)
         nt_constraint = no_shifts - sum(primo_has_shift[primo])
         model += pulp.LpConstraint(e=nt_constraint, sense=pulp.LpConstraintEQ, rhs=0, name=f'no_shifts_constraint_{primo.rol}')
-    
+
     # Esta restricción evita que algún primo salga muy perjudicado
     mean = sum(satisfaction)/len(satisfaction)
     std = sum(var - mean for var in satisfaction)/len(satisfaction)
-    model += std <= 2*primos_per_shift/len(satisfaction)
+    model += std <= 2*(total_shifts / len(primos_per_shift))/len(satisfaction)
 
     # Resolvemos
     model.solve()
@@ -75,11 +86,21 @@ def get_schedule(primos: Iterable, no_blocks: int, primos_per_shift: int = 2):
         schedule[primo] = parse_schedule([bool(variables[f'has_shift_{primo.rol}_{shift}']) for shift in shifts], no_blocks)
     return schedule
 
+
 if __name__ == '__main__':
-    from primos import primos
-    result = get_schedule(primos, 7)
+    from primos import primos, new_primos
+
+    req = [
+            [0, 2, 2, 2, 1, 2, 2, 1],
+            [1, 2, 1, 2, 2, 2, 2, 1],
+            [0, 2, 2, 2, 1, 2, 2, 1],
+            [0, 2, 2, 2, 1, 2, 2, 1],
+            [0, 2, 2, 2, 2, 1, 1, 1],
+            ]
+    result = get_schedule(primos, new_primos, req)
+
     for primo in primos:
-        print(primo.rol, result[primo])
+        print(primo.nick, result[primo])
     with open('primos.csv', 'w', encoding='utf-8') as file:
         for primo in primos:
             file.write(f'{primo.rol};{primo.mail};{primo.name};{primo.nick};{result[primo]}\n')
